21923738328d1daa7ac1d2f33393b48f6ad74900
Brandon Purvis <bcp7cc@virginia.edu>
2016-04-17 20:15:30 -0400

    Squids attack or flee based on strength differential.

--- src/objects/squid.js
+++ src/objects/squid.js
@@ -7,6 +7,7 @@ class Squid extends PhysicsSprite {
 		this.applyGravity = false;
 		this.updateTimer = new GameClock();
 		this.moveTimer = new GameClock();
+		this.moveStatus = 'wandering' // 'wandering' 'attacking' 'fleeing'
 		this.rotationDampener = .5; // variabilitiy of rotation
 		this.movePower = 0.1;  // speed of movement
 		this.direction = Math.random() * 2 * Math.PI; // random initial direction
@@ -37,9 +38,28 @@ class Squid extends PhysicsSprite {
     }
 
     update(){
-	    this.hitbox = this.makeHitbox();	
+	    this.hitbox = this.makeHitbox();
+	    this.updateMoveStatus();	
 		if (this.delayed && this.delayElapsed()){this.deactivateMoveDelay();}
 		super.update();
+    }
+
+    updateMoveStatus(){
+    	// if player is close and squid is weaker then flee[fleeing].
+    	// if player is close and squid is stronger then attack[attacking].
+    	// otherwise wander [wandering]
+    	var CLOSE_THRESHOLD = 100;  // pixels
+    	var current_distance = Math.sqrt(Math.pow(this.getX() - PLAYER.getX(), 2) + Math.pow(this.getY() - PLAYER.getY(), 2));
+    	if (current_distance < CLOSE_THRESHOLD){
+    		if (this.strength < PLAYER.strength){
+    			this.moveStatus = 'fleeing';
+    		}
+    		if (this.strength > PLAYER.strength){
+    			this.moveStatus = 'attacking';
+    		}
+    	} else {
+    		this.moveStatus = 'wandering';
+    	}
     }
 
 	draw(g){
@@ -47,11 +67,24 @@ class Squid extends PhysicsSprite {
 		this.stayInside(canvasHitbox);
 		g.font = "30px Helvetica";
 		g.fillText(Math.round(this.strength), this.x + 5, this.y + 5);
+		// DEBUGGING
+		// g.fillText(Math.round(this.direction / Math.PI * 180), this.x + 15, this.y + 15);
+		// g.fillText(this.moveStatus, this.x + 35, this.y + 35);
 		var scale = this.strength / PLAYER.strength;
 		this.scaleX = scale;
 		this.scaleY = scale;
 		this.setPivotPoint([this.getWidth() / 2, this.getHeight() / 2])
 	}
+
+
+    calculatePlayerRotation(){
+    	// Return the rotation required to face the player in radians.
+    	var dpx = PLAYER.getX() - this.getX();
+		var dpy = PLAYER.getY() - this.getY();
+		var angle =  Math.atan(dpy / dpx);
+		if (dpx <= 0){ angle += Math.PI};
+		return angle;
+    }
 
     // redefine move
     // Called after every draw
@@ -59,13 +92,18 @@ class Squid extends PhysicsSprite {
 		// get time since last update
 		var delta_t = this.updateTimer.getElapsedTime();
 		this.updateTimer.resetGameClock();
-
-		// randomly choose a direction to move
 		this.vx = (Math.cos(this.direction))*this.movePower;
 		this.vy = (Math.sin(this.direction))*this.movePower;
-		this.setRotation((this.direction / (2*Math.PI))*360);	
-		if (Math.random() < .1){ this.direction += this.rotationDampener*(Math.random() - .5) * (Math.PI); }
+		this.setRotation(this.direction / Math.PI * 180);	
 		
+		// Update Squid rotation
+		if (this.moveStatus == 'wandering'){
+			if (Math.random() < .1){ this.direction += this.rotationDampener*(Math.random() - .5) * (Math.PI); }
+		} else if (this.moveStatus == 'attacking') {
+			this.direction = this.calculatePlayerRotation();  // Face towards player
+		} else if (this.moveStatus == 'fleeing'){
+			this.direction = this.calculatePlayerRotation() - Math.PI; // Face away from player
+		}
 		// update position
 		var newX = (this.getX()) + (this.vx * delta_t);
 		var newY = (this.getY()) + (this.vy * delta_t);
